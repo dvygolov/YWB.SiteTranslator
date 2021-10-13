96225f5b2e718790893ac389b18f6d885a3ebf73
Daniel Vygolov <rattack@gmail.com>
2021-10-13 22:02:46 +0400

    Console Encoding

--- YWB.SiteTranslator/Program.cs
+++ YWB.SiteTranslator/Program.cs
@@ -8,8 +8,8 @@ namespace YWB.SiteTranslator
     {
         static async Task Main(string[] args)
         {
-            Console.InputEncoding = System.Text.Encoding.Unicode;
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Sites Translator by Yellow Web ver 1.0c");
             Console.WriteLine("If you like this software, please, donate!");
             DonationHelper.Info();
